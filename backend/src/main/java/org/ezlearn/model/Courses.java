f452fa5bf946c02a8bf2c2580955f432aa1aba34
Peter Hsu <yuchehsu2336@gmail.com>
2025-01-02 14:22:34 +0800

    Add backend models with relationships

---
+++ backend/src/main/java/org/ezlearn/model/Courses.java
@@ -0,0 +1,89 @@
+package org.ezlearn.model;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Courses {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long courseId;
+	private Integer price;
+	private String courseName;
+	private String courseIntro;
+	private String courseType;
+	private byte[] courseImg;
+	
+	public Long getCourseId() {
+		return courseId;
+	}
+
+	public void setCourseId(Long courseId) {
+		this.courseId = courseId;
+	}
+
+	public Integer getPrice() {
+		return price;
+	}
+
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+
+	public String getCourseName() {
+		return courseName;
+	}
+
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+
+	public String getCourseIntro() {
+		return courseIntro;
+	}
+
+	public void setCourseIntro(String courseIntro) {
+		this.courseIntro = courseIntro;
+	}
+
+	public String getCourseType() {
+		return courseType;
+	}
+
+	public void setCourseType(String courseType) {
+		this.courseType = courseType;
+	}
+
+	public byte[] getCourseImg() {
+		return courseImg;
+	}
+
+	public void setCourseImg(byte[] courseImg) {
+		this.courseImg = courseImg;
+	}
+
+	@ManyToOne
+	@JoinColumn(name = "teacher_id")
+	private UserInfo teachers;
+
+	public UserInfo getTeachers() {
+		return teachers;
+	}
+
+	public void setTeachers(UserInfo teachers) {
+		this.teachers = teachers;
+	}
+	
+	
+
+}
---
+++ backend/src/main/java/org/ezlearn/model/Users.java
@@ -0,0 +1,53 @@
+package org.ezlearn.model;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Users {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long userId;
+	private String email;
+	private String password;
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	@OneToMany(mappedBy = "users")
+	private List<PurchasedCourses> purchasedCourses;
+	
+	@OneToOne(mappedBy = "users", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private UserInfo userInfo;
+	
+}
