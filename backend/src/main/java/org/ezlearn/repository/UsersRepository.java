a4e6c8bf2967f8788e779547f8f1dee9e16fca48
Peter Hsu <yuchehsu2336@gmail.com>
2025-01-06 00:59:22 +0800

    Add UsersRepository

---
+++ backend/src/main/java/org/ezlearn/repository/UsersRepository.java
@@ -0,0 +1,10 @@
+package org.ezlearn.repository;
+
+import org.ezlearn.model.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsersRepository extends JpaRepository<Users, Long> {
+
+}
