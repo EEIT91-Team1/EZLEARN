fb984715bd85f3d72425be5640e3cb8aca419acb
Peter Hsu <yuchehsu2336@gmail.com>
2025-01-07 11:41:02 +0800

    Modify purchased courses API to use session parameters

--- backend/src/main/java/org/ezlearn/controller/PurchasedCoursesController.java
+++ backend/src/main/java/org/ezlearn/controller/PurchasedCoursesController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpSession;
+
 @RestController
 @RequestMapping("/purchased-courses")
 public class PurchasedCoursesController {
@@ -20,11 +22,9 @@ public class PurchasedCoursesController {
 	private PurchasedCoursesService purchasedCoursesService;
 	
 	@CrossOrigin(origins = "http://127.0.0.1:5500")
-	@GetMapping("/{userId}")
-    public List<PurchasedCourses> getPurchasedCoursesByUser(@PathVariable Long userId) {
-        Users users = new Users();
-        users.setUserId(userId); // 假設有 userId 的資料
-        System.out.println(userId);
+	@GetMapping("/my-courses")
+    public List<PurchasedCourses> getPurchasedCoursesByUser(HttpSession session) {
+        Users users = (Users)session.getAttribute("user");
         return purchasedCoursesService.getPurchasedCoursesByUsers(users);
     }
 
