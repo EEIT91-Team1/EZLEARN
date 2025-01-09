27ee11be86cd3f9997f4167e4866a0b0cb39b16e
Peter Hsu <yuchehsu2336@gmail.com>
2025-01-09 23:06:36 +0800

    Add QuestionsRepository to query questions table by lesson

---
+++ backend/src/main/java/org/ezlearn/repository/QuestionsRepository.java
@@ -0,0 +1,15 @@
+package org.ezlearn.repository;
+
+import java.util.List;
+
+import org.ezlearn.model.Lessons;
+import org.ezlearn.model.Questions;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuestionsRepository extends JpaRepository<Questions, Long>{
+
+	List<Questions> findByLesson(Lessons lesson);
+	
+}
