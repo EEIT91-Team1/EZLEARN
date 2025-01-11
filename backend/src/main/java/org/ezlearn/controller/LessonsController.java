7fa722015869b5371c7709715251af265fa376be
Peter Hsu <yuchehsu2336@gmail.com>
2025-01-12 00:53:10 +0800

    Require authentication for LessonsController API usage

--- backend/src/main/java/org/ezlearn/controller/LessonsController.java
+++ backend/src/main/java/org/ezlearn/controller/LessonsController.java
@@ -1,15 +1,18 @@
 package org.ezlearn.controller;
 
-import java.util.List;
-
 import org.ezlearn.model.Courses;
-import org.ezlearn.model.Lessons;
 import org.ezlearn.service.LessonsService;
+import org.ezlearn.service.PurchasedCoursesService;
+import org.ezlearn.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("/courses")
@@ -18,11 +21,28 @@ public class LessonsController {
 	@Autowired
 	private LessonsService lessonsService;
 	
+	@Autowired
+	private UsersService usersService;
+	
+	@Autowired
+	private PurchasedCoursesService purchasedCoursesService;
+	
+	@Autowired
+	private HttpSession httpSession;
+	
 	@GetMapping("/{courseId}/lessons")
-	public List<Lessons> getLessonsByCourses(@PathVariable Long courseId) {
+	public ResponseEntity<?> getLessonsByCourses(@PathVariable Long courseId) {
+		if (usersService.islogin(httpSession) == false) {
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthenticated");
+		}
+		
+		if(purchasedCoursesService.isPurchased(httpSession, courseId) == false) {
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized");
+		}
+		
 		Courses courses = new Courses();
 		courses.setCourseId(courseId);
-		return lessonsService.getLessonsByCourses(courses);
+		return ResponseEntity.ok(lessonsService.getLessonsByCourses(courses));
 	}
 
 }
