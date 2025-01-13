603f081ff7062dc8ff7658c78501857e378a69fc
Ennyou <nyu3105305@gmail.com>
2025-01-13 14:38:04 +0800

    修改頁面樣式

--- frontend/js/search.js
+++ frontend/js/search.js
@@ -27,9 +27,7 @@ function option(title) {
   $(`#${title}`).on("click", () => {
     if ($(`#${title}Option`).hasClass("hidden")) {
       $(`#${title}Option`).removeClass("hidden");
-      $(`#${title}`)
-        .find("i")
-        .removeClass("bi-chevron-down");
+      $(`#${title}`).find("i").removeClass("bi-chevron-down");
       $(`#${title}`).find("i").addClass("bi-chevron-up");
     } else {
       $(`#${title}Option`).addClass("hidden");
@@ -80,28 +78,14 @@ $("#btnSearch").on("click", (event) => {
 //後端
 //設定參數
 function getSearchParams() {
-  let urlParams = new URLSearchParams(
-    window.location.search
-  );
+  let urlParams = new URLSearchParams(window.location.search);
   let query = urlParams.get("query") || "";
-  let page = urlParams.get("page")
-    ? "&page=" + urlParams.get("page")
-    : "";
-  let type = urlParams.get("type")
-    ? "&type=" + urlParams.get("type")
-    : "";
-  let price = urlParams.get("price")
-    ? "&price=" + urlParams.get("price")
-    : "";
-  let rate = urlParams.get("rate")
-    ? "&rate=" + urlParams.get("rate")
-    : "";
-  let sort = urlParams.get("sort")
-    ? "&sort=" + urlParams.get("sort")
-    : "";
-  let pageNumber = urlParams.get("page")
-    ? urlParams.get("page")
-    : "1";
+  let page = urlParams.get("page") ? "&page=" + urlParams.get("page") : "";
+  let type = urlParams.get("type") ? "&type=" + urlParams.get("type") : "";
+  let price = urlParams.get("price") ? "&price=" + urlParams.get("price") : "";
+  let rate = urlParams.get("rate") ? "&rate=" + urlParams.get("rate") : "";
+  let sort = urlParams.get("sort") ? "&sort=" + urlParams.get("sort") : "";
+  let pageNumber = urlParams.get("page") ? urlParams.get("page") : "1";
   return {
     query,
     page,
@@ -181,18 +165,14 @@ async function loadResults() {
                <h1>${item.courseName}</h1>
                <h2>${item.courseIntro}</h2>
                <h3>${item.teacherName}</h3>
-               <h4>${rateToStars(item.courseRate)}（${
-            item.courseRate
-          }）</h4>
+               <h4>${rateToStars(item.courseRate)}（${item.courseRate}）</h4>
              </div>
              <div class="divPrice">
                <p>$${item.price}</p>
                </div>
           </div></a>
                <div class="text-4xl text-end absolute bottom-4 right-8 z-10">
-               <i onclick="addWishList(${
-                 item.courseId
-               })" id="wish${
+               <i onclick="addWishList(${item.courseId})" id="wish${
             item.courseId
           }" class="bi bi-heart-fill text-white hover:text-red-400 duration-300 mr-2 cursor-pointer"></i>
                
@@ -211,10 +191,7 @@ async function loadResults() {
     },
   }).done((data) => {
     $.each(data, function (idx, item) {
-      $(`#wish${item.courseId}`).css(
-        "color",
-        "rgb(248 113 113)"
-      );
+      $(`#wish${item.courseId}`).css("color", "rgb(248 113 113)");
       $(`#wish${item.courseId}`).addClass("isWish");
     });
   });
@@ -227,9 +204,11 @@ function getCount() {
     url: url,
     method: "GET",
   }).done((data) => {
-    $("#queryText").text(
-      `「${params.query}」有 ${data.courseCount} 個結果`
-    );
+    if (params.query == "") {
+      $("#queryText").text(`「所有分類」有 ${data.courseCount} 個結果`);
+    } else {
+      $("#queryText").text(`「${params.query}」有 ${data.courseCount} 個結果`);
+    }
     $("#languageLabel").text(`(${data.languageCount})`);
     $("#programLabel").text(`(${data.programCount})`);
     $("#cookLabel").text(`(${data.cookCount})`);
@@ -239,102 +218,73 @@ function getCount() {
   });
 }
 
-loadResults();
 getCount();
+loadResults();
 
 //-------------------------------------------------------------
 //type
 $("input[name ='type']").change(function () {
   let typeValue = $(this).val();
-  const urlParams = new URLSearchParams(
-    window.location.search
-  );
+  const urlParams = new URLSearchParams(window.location.search);
   urlParams.delete("page");
   if (typeValue === "all") {
     urlParams.delete("type");
   } else {
     urlParams.set("type", typeValue);
   }
-  history.replaceState(
-    null,
-    "",
-    "?" + urlParams.toString()
-  );
+  history.replaceState(null, "", "?" + urlParams.toString());
   loadResults();
 });
 //-------------------------------------------------------------
 //rate
 $("input[name ='rate']").change(function () {
   let rateValue = $(this).val();
-  const urlParams = new URLSearchParams(
-    window.location.search
-  );
+  const urlParams = new URLSearchParams(window.location.search);
   urlParams.delete("page");
   if (rateValue === "all") {
     urlParams.delete("rate");
   } else {
     urlParams.set("rate", rateValue);
   }
-  history.replaceState(
-    null,
-    "",
-    "?" + urlParams.toString()
-  );
+  history.replaceState(null, "", "?" + urlParams.toString());
   loadResults();
 });
 //------------------------------------------------------------
 //Price
-$('input[type="radio"][name="price"]').on(
-  "change",
-  function () {
-    if ($(this).val() === "choose") {
-      $("#divPriceRange").removeClass("hidden");
-    } else {
-      $("#divPriceRange").addClass("hidden");
-      $("#inputPrice").val("");
-      $("#showPrice").text("");
-      let priceValue = $("#inputPrice").val();
-      const urlParams = new URLSearchParams(
-        window.location.search
-      );
-      urlParams.delete("page");
-      urlParams.delete("price");
-      history.replaceState(
-        null,
-        "",
-        "?" + urlParams.toString()
-      );
-      loadResults();
-    }
+$('input[type="radio"][name="price"]').on("change", function () {
+  if ($(this).val() === "choose") {
+    $("#divPriceRange").removeClass("hidden");
+  } else {
+    $("#divPriceRange").addClass("hidden");
+    $("#inputPrice").val("");
+    $("#showPrice").text("");
+    let priceValue = $("#inputPrice").val();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.delete("page");
+    urlParams.delete("price");
+    history.replaceState(null, "", "?" + urlParams.toString());
+    loadResults();
   }
-);
+});
 $("#inputPrice").on("input", () => {
   $("#showPrice").text($("#inputPrice").val() + "元以下");
 });
 $("#inputPrice").change(() => {
   let priceValue = $("#inputPrice").val();
-  const urlParams = new URLSearchParams(
-    window.location.search
-  );
+  const urlParams = new URLSearchParams(window.location.search);
   urlParams.delete("page");
   if (priceValue === "all") {
     urlParams.delete("price");
   } else {
     urlParams.set("price", priceValue);
   }
-  history.replaceState(
-    null,
-    "",
-    "?" + urlParams.toString()
-  );
+  history.replaceState(null, "", "?" + urlParams.toString());
   loadResults();
 });
 //-----------------------------------------------------------------
 //排序
 function sort() {
-  let urlParams = new URLSearchParams(
-    window.location.search
-  );
+  let urlParams = new URLSearchParams(window.location.search);
   switch (urlParams.get("sort")) {
     case "rate":
       $("#sortRate").addClass("sortTarget");
@@ -358,16 +308,10 @@ function sortChoose(param, val) {
     $("a.sort").removeClass("sortTarget");
     $(this).addClass("sortTarget");
     let sortValue = val;
-    const urlParams = new URLSearchParams(
-      window.location.search
-    );
+    const urlParams = new URLSearchParams(window.location.search);
     urlParams.delete("page");
     urlParams.set("sort", sortValue);
-    history.replaceState(
-      null,
-      "",
-      "?" + urlParams.toString()
-    );
+    history.replaceState(null, "", "?" + urlParams.toString());
     loadResults();
   });
 }
@@ -379,16 +323,10 @@ sortChoose($("#sortPriceB"), "pb");
 $("#sortNew").click(function () {
   $("a.sort").removeClass("sortTarget");
   $(this).addClass("sortTarget");
-  const urlParams = new URLSearchParams(
-    window.location.search
-  );
+  const urlParams = new URLSearchParams(window.location.search);
   urlParams.delete("page");
   urlParams.delete("sort");
-  history.replaceState(
-    null,
-    "",
-    "?" + urlParams.toString()
-  );
+  history.replaceState(null, "", "?" + urlParams.toString());
   loadResults();
 });
 //-----------------------------------------------------------------
@@ -398,15 +336,9 @@ $("#btnPageNext").click(() => {
   if (hasNextPage == false) {
     alert("已經到最後一頁囉");
   } else {
-    const urlParams = new URLSearchParams(
-      window.location.search
-    );
+    const urlParams = new URLSearchParams(window.location.search);
     urlParams.set("page", parseInt(params.pageNumber) + 1);
-    history.replaceState(
-      null,
-      "",
-      "?" + urlParams.toString()
-    );
+    history.replaceState(null, "", "?" + urlParams.toString());
     loadResults();
     $("html, body").animate({ scrollTop: 0 });
   }
@@ -416,15 +348,9 @@ $("#btnPagePrev").click(() => {
   if (parseInt(params.pageNumber) == 1) {
     alert("你已經在第一頁了");
   } else {
-    const urlParams = new URLSearchParams(
-      window.location.search
-    );
+    const urlParams = new URLSearchParams(window.location.search);
     urlParams.set("page", parseInt(params.pageNumber) - 1);
-    history.replaceState(
-      null,
-      "",
-      "?" + urlParams.toString()
-    );
+    history.replaceState(null, "", "?" + urlParams.toString());
     loadResults();
     $("html, body").animate({ scrollTop: 0 });
   }
