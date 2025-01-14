c57af26409f41da95a10f278d6d2c2f0c0888e20
Peter Hsu <yuchehsu2336@gmail.com>
2025-01-14 19:10:02 +0800

    Remove @JsonIdentityInfo from UserInfo model

--- backend/src/main/java/org/ezlearn/model/UserInfo.java
+++ backend/src/main/java/org/ezlearn/model/UserInfo.java
@@ -1,8 +1,5 @@
 package org.ezlearn.model;
 
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
@@ -12,7 +9,6 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "user_info")
-@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "userId")
 public class UserInfo {
 	
 	@Id
